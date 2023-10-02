0ec0c4d367905599fba235990361d1fc1fe716d5
Param2k23 <125284329+Param2k23@users.noreply.github.com>
2023-10-02 16:16:12 +0530

    .

--- app/model/usersModel.js
+++ app/model/usersModel.js
@@ -3,55 +3,42 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     firstName:{
         type: String,
-        required: true
     },
     lastName:{
         type: String,
-        required: true
     },
     email:{
         type: String,
-        required: true
     },
     password:{
         type: String,
-        required: true
     },
     contactNum:{
-        type: String,
-        required: true
+        type: String
     },
     address:{
         type: String,
-        required: true
     },
     state:{
-        type: String,
-        required: true
+        type: String
     },
     city:{
-        type: String,
-        required: true
+        type: String
     },
     pinCode:{
-        type: String,
-        required: true
+        type: String
     },
     activeInd:{
-        type: String,
-        required: true
+        type: String
     },
     createdAt:{
-        type: String, 
-        required: true   
+        type: String   
     },
     profileURL:{
-        type: String,
-        required: true 
+        type: String
     },
     role:{
-        type: String,
-        required: true
+        type: String
     }
 });
 
